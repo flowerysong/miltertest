33c3e003395c44d1966cf51c56a54d54dff853ff
Paul Arthur <paul.arthur@flowerysong.com>
2024-10-09 18:28:55 +0000

    Update copyright headers

--- libmilter/codec.py
+++ libmilter/codec.py
@@ -1,4 +1,5 @@
 # Copyright 2011 Chris Siebenmann
+# Copyright 2024 Paul Arthur MacIain
 # GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 # Encode and decode the milter protocol.
